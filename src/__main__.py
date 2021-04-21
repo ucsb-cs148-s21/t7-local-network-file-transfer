b9e0a49ce084016c33ed7b6ce8e584f5ee5f6807
Kevin Pham <k_pham@ucsb.edu>
2021-04-20 21:08:11 -0700

    added upload html button and functionality

--- src/__main__.py
+++ src/__main__.py
@@ -1,11 +1,14 @@
-
 import sys
 import webbrowser
+import os
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 from gevent.pywsgi import WSGIServer
 from PyQt5.QtWidgets import QApplication, QGridLayout, QLabel, QPushButton, QWidget
+from werkzeug.utils import secure_filename
+from werkzeug.middleware.shared_data import SharedDataMiddleware
 
+UPLOAD_FOLDER = 'downloads'
 APP_NAME = 'loft'
 HOST = '0.0.0.0'
 PORT = 2402
@@ -13,6 +16,8 @@ PORT = 2402
 gui = QApplication(sys.argv)
 app = Flask(APP_NAME)
 server = WSGIServer((HOST, PORT), app)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 
 def start_server(app, window):
@@ -55,6 +60,47 @@ def quit_server():
     server.stop()
     return 'good-bye'
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return render_template('index.html')
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+app.add_url_rule('/uploads/<filename>', 'uploaded_file',
+                 build_only=True)
+app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
+    '/uploads':  app.config['UPLOAD_FOLDER']
+})
 
 if __name__ == '__main__':
     main()
