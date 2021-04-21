3455c31b5f9f3df4df15317d4632b3002e3f087f
Kevin Pham <k_pham@ucsb.edu>
2021-04-21 14:41:03 -0700

    removed unnecessary code, and added code to automatically create an uploads folder

--- src/__main__.py
+++ src/__main__.py
@@ -8,7 +8,10 @@ from PyQt5.QtWidgets import QApplication, QGridLayout, QLabel, QPushButton, QWid
 from werkzeug.utils import secure_filename
 from werkzeug.middleware.shared_data import SharedDataMiddleware
 
-UPLOAD_FOLDER = 'downloads'
+UPLOAD_FOLDER = 'uploads'
+if(~os.path.isfile(UPLOAD_FOLDER)):
+    os.mkdir('uploads')
+
 APP_NAME = 'loft'
 HOST = '0.0.0.0'
 PORT = 2402
@@ -80,7 +83,9 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return render_template('index.html')
+            #return render_template('index.html')
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
     return '''
     <!doctype html>
     <title>Upload new File</title>
@@ -88,19 +93,12 @@ def upload_file():
     <form method=post enctype=multipart/form-data>
       <input type=file name=file>
       <input type=submit value=Upload>
-    </form>
-    '''
+    </form>'''
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'],
                                filename)
-
-app.add_url_rule('/uploads/<filename>', 'uploaded_file',
-                 build_only=True)
-app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
-    '/uploads':  app.config['UPLOAD_FOLDER']
-})
 
 if __name__ == '__main__':
     main()
