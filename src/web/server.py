27a4e23ba07875665ee48537268391eb667ad533
Ethan Wu <ethanwu@cs.ucsb.edu>
2021-04-23 14:24:19 -0700

    Remove unused code, fix bug

--- src/web/server.py
+++ src/web/server.py
@@ -2,7 +2,7 @@
 import os
 from threading import Thread
 
-from flask import Flask, request
+from flask import Flask
 from werkzeug.serving import run_simple
 
 
@@ -11,13 +11,12 @@ class Server:
     Abstract layer over the server.
     '''
 
-    def __init__(self, host: str = 'localhost', port: int = 5000, config: dict = None):
+    def __init__(self, host: str = 'localhost', port: int = 5000, config: dict = {}):
         # the flask WSGI application
         self.flask = Flask(__name__)
         self.flask.config.update(config)
-        # self.server = WSGIServer((host, port), self.flask)
         self.thread = Thread(
-            target=lambda *args: run_simple(host, port, self.flask), daemon=True)
+            target=lambda: run_simple(host, port, self.flask), daemon=True)
 
         register_blueprints(self.flask)
 
@@ -28,11 +27,9 @@ class Server:
         '''Run the server.'''
         if not self.thread.is_alive():
             self.thread.start()
-        # self.server.serve_forever()
 
     def stop(self):
         '''TODO: Stop the server.'''
-        # self.server.stop()
 
     def open_index_in_browser(self):
         '''Open the index in the user's default browser.'''
