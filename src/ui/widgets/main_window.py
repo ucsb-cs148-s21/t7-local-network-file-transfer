3874d927318dd36beca1cbddbd13dfb90fd00267
Andrew Tran <andrew_tran@ucsb.edu>
2021-04-25 02:29:40 -0700

    shows the address

--- src/ui/widgets/main_window.py
+++ src/ui/widgets/main_window.py
@@ -1,7 +1,6 @@
 
 from PyQt5.QtWidgets import *
 
-
 def create_main_window(title: str, callbacks) -> QWidget:
     '''
     Create the main application window layout.
@@ -17,7 +16,7 @@ def create_main_window(title: str, callbacks) -> QWidget:
     window.move(400, 400)
     layout = QGridLayout(window)
 
-    welcome = QLabel(text='<p>Welcome to Loft!</p>')
+    welcome = QLabel(text='<p>Welcome to Loft! This is the Host device.</p>')
     start_button = QPushButton(text='Start Server')
     start_button.clicked.connect(callbacks['start'])
 
@@ -27,10 +26,21 @@ def create_main_window(title: str, callbacks) -> QWidget:
     open_server_button = QPushButton(text='Open Server Page')
     open_server_button.clicked.connect(callbacks['link'])
 
+    connect_msg = QLabel(text='Connect by opening a browser on the client device and going to \nhttp://'+get_connect_info())
+
     layout.addWidget(welcome, 0, 0)
     layout.addWidget(start_button, 1, 0)
     layout.addWidget(stop_button, 2, 0)
     layout.addWidget(open_server_button, 3, 0)
+    layout.addWidget(connect_msg, 4, 0)
 
     window.setTabOrder(start_button, welcome)
     return window
+
+def get_connect_info():
+    # from ...__init__ import PORT
+    from utils import getIP
+    # return(getIP.get_ip_thru_gateway() + string(PORT))
+    return (getIP.get_ip_thru_gateway() + ":2402")
+
+
--- src/utils/getIP.py
+++ src/utils/getIP.py
@@ -8,12 +8,11 @@ def get_ip_thru_gateway():
     to be determined.
     Returns ip as a string.
     '''
-    # Get the user's router ip address https://pypi.org/project/netifaces/
-    ​gateway = netifaces.gateways()['default'][netifaces.AF_INET][0]
-    
-    # Connect socket to the router
+
+    gateway = netifaces.gateways()['default'][netifaces.AF_INET][0]	# https://pypi.org/project/netifaces/
+
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect((gateway, 80))					# Connect to gateway (router) to get local facing ipaddress
-    
-    ipaddress = s.getsockname()[0])
-    return ipaddress
+    address = s.getsockname()[0]
+    s.close()
+    return address
