e5a939acb1264e1b6c0e483d2817e0fb3a9d617a
at527 <59618726+at527@users.noreply.github.com>
2021-04-26 00:36:51 -0700

    received to receive

--- src/__init__.py
+++ src/__init__.py
@@ -3,11 +3,8 @@ APP_NAME: str = 'loft'
 HOST: str = '0.0.0.0'
 PORT: int = 2402
 
-# working directory for the app (maybe make hidden in the future)
-APP_FOLDER: str = '~/loftApp'
-
 # folder where files received to the host from the client go
-RECEIVED_FOLDER: str = '~/loftApp/received'
+RECEIVE_FOLDER: str = '~/Loft/receive'
 
 # folder where files to send to the client from the host go
-SEND_FOLDER: str = '~/loftApp/send'
+SEND_FOLDER: str = '~/Loft/send'
