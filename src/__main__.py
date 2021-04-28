a5afc614ab5acbf53cce0bff2c0316c4e1a2e961
Ethan Wu <ethanwu@cs.ucsb.edu>
2021-04-28 15:59:46 -0700

    Fix PR problems
    - Fixes conflicts caused by multiple PRs modifying the same files the
      same way

--- src/__main__.py
+++ src/__main__.py
@@ -1,18 +1,18 @@
 
 import os
 
-from __init__ import APP_NAME, RECEIVE_FOLDER, HOST, PORT
+from __init__ import APP_NAME, DOWNLOADS_FOLDER, HOST, PORT
 from ui import Gui
 
-RECEIVE_FOLDER = os.path.expanduser(RECEIVE_FOLDER)
+DOWNLOADS_FOLDER = os.path.expanduser(DOWNLOADS_FOLDER)
 
 def main():
     '''Entry point for the application.'''
-    
-    if not os.path.exists(RECEIVE_FOLDER):
-        os.makedirs(RECEIVE_FOLDER, exist_ok=True)
 
-    gui = Gui(APP_NAME, HOST, PORT, {'receive_folder': RECEIVE_FOLDER})
+    if not os.path.exists(DOWNLOADS_FOLDER):
+        os.makedirs(DOWNLOADS_FOLDER, exist_ok=True)
+
+    gui = Gui(APP_NAME, HOST, PORT, {'downloads_folder': DOWNLOADS_FOLDER})
     gui.run_and_exit()
 
 
