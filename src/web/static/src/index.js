1ef7775d35358b92e63be93cd2049a751d63ce04
Kevin Pham <k_pham@ucsb.edu>
2021-05-11 12:01:08 -0700

    added logic for when file is changed on web page

--- src/web/static/src/index.js
+++ src/web/static/src/index.js
@@ -1,10 +1,15 @@
 let fileInput = document.querySelector('#file-selector');
 fileInput.onchange = function(e){
     let filename = fileInput.files[0].name;
-    let newDiv = document.createElement("div");
-    newDiv.innerHTML= `${filename} selected.`;
-    newDiv.className = 'button file';
-    document.querySelector('#file-selector-div').parentNode.insertBefore(newDiv,document.querySelector('#file-selector-div').nextSibling);
-    console.log("hello");
-    //document.querySelector('#file-selector-label').innerHTML=`${filename} selected.`;
+    if(!document.querySelector('#selected-file')){
+        let newDiv = document.createElement("div");
+        newDiv.id = "selected-file"
+        newDiv.innerHTML= `${filename} selected.`;
+        newDiv.className = 'button file';
+        document.querySelector('#file-selector-div').parentNode.insertBefore(newDiv,document.querySelector('#file-selector-div').nextSibling);
+    }
+    else{
+        let newDiv = document.querySelector('#selected-file');
+        newDiv.innerHTML = `${filename} selected.`;
+    }
 }
