1c41778352bd564ca117280a95dee5500634e04e
Ethan Wu <ethanwu@cs.ucsb.edu>
2021-05-12 13:56:13 -0700

    Add `.bubble` class

--- src/web/static/src/index.js
+++ src/web/static/src/index.js
@@ -1,17 +1,18 @@
 let fileInput = document.querySelector('#file-selector');
-fileInput.onchange = function(e){
+fileInput.onchange = function (e) {
     let filename = fileInput.files[0].name;
-    if(filename.length >= 8){
-        filename = filename.slice(0,7) + '...';
+    if (filename.length >= 8) {
+        filename = filename.slice(0, 7) + '...';
     }
-    if(!document.querySelector('#selected-file-1')){
+    if (!document.querySelector('#selected-file-1')) {
         let newDiv = document.createElement("div");
         newDiv.id = "selected-file-1"
-        newDiv.innerHTML= `${filename} selected.`;
-        newDiv.className = 'selected-file';
-        document.querySelector('#file-selector-div').parentNode.insertBefore(newDiv,document.querySelector('#file-selector-div').nextSibling);
+        newDiv.innerHTML = `${filename} selected.`;
+        newDiv.classList.add('selected-file');
+        newDiv.classList.add('bubble');
+        document.querySelector('#file-selector-div').parentNode.insertBefore(newDiv, document.querySelector('#file-selector-div').nextSibling);
     }
-    else{
+    else {
         let newDiv = document.querySelector('#selected-file');
         newDiv.innerHTML = `${filename} selected.`;
     }
