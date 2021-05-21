36dc18437a9423ccc6cda4ecbd8657d2373ca982
Andrew Tran <andrew_tran@ucsb.edu>
2021-05-21 02:38:03 -0700

    login page shows, but browser saves it or something

--- loft/web/blueprints/landing.py
+++ loft/web/blueprints/landing.py
@@ -1,11 +1,25 @@
 
 from flask import Blueprint, render_template
-
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
 
 def landing():
     landing = Blueprint('landing', __name__)
 
+    auth = HTTPBasicAuth()
+
+    users = {
+        "loft": generate_password_hash("test")
+    }
+
+    @auth.verify_password
+    def verify_password(username, password):
+        if username in users and \
+            check_password_hash(users.get(username), password):
+            return username
+
     @landing.route('/')
+    @auth.login_required
     def index():
         return render_template('pages/index.html')
 
